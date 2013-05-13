816b6150eb93ea5f050c4ab95eddfaa8c303c367
Fabien Lalanne <lalanne.fabien@gmail.com>
2013-05-13 19:27:07 +0200

    fix fatal after add config file

--- upAsDownloaded.php
+++ upAsDownloaded.php
@@ -1,7 +1,10 @@
 <?php
 	require_once 'lib/BetaSerie.class.php';
-	require_once 'lib/Tools.lib.php';
+	require_once 'config.php';
 	
+	$betaSerie = new BetaSerie();
+	$betaSerie->memberAuthentication(LOGIN, MDP);
+
 	$serie = $_POST['serie'];
 	$season = $_POST['season']; 
 	$episode = $_POST['episode'];
